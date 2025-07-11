2ed767fd9998cd83a5a873e3400edca63aeba1c7
Fati3 <fatigueee56@gmail.com>
2025-07-11 11:17:35 -0600

    En el main agregue codigo.
    Agregue el punto de entrada principal de una aplicación Java.

--- src/main/java/com/mycompany/eduquizz/EduQuizz.java
+++ src/main/java/com/mycompany/eduquizz/EduQuizz.java
@@ -8,4 +8,12 @@ package com.mycompany.eduquizz;
  *
  * @author EMILY
  */
+public class EduQuizz {
 
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+                new login().setVisible(true);
+            
+        });
+    }
+}
